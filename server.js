40e29ab05776e9f605d95fd97a801ac032010c80
Akashzealzoft <akash@zealzoft.com>
2023-11-27 15:43:41 +0530

    authentication module compelete

--- server.js
+++ server.js
@@ -21,17 +21,18 @@ const user = require("./routes/user.route");
 const register = require("./routes/registerRoute");
 
 // Express Inbuilt middleware
-
 server.use(express.json()); // Used in passing application/json data
 server.use(express.urlencoded({ extended: false })); // Used in passing form
 server.use(cookieParser()); // Used in setting the cookies parser
 
 //server use
 
-// Routes for API
+//auth routes
+server.use("/api/auth", register);
 
-server.use("/api", register);
+//error handlers
 server.use(notFound);
 server.use(errorHandler);
+
 // Creating the server
 server.listen(PORT, () => console.log("Server is running on port " + PORT));
